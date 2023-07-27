baaeefd8cbe85b49ad427e50e97f0ef44d0a7608
uoethan <72795247+uoethan@users.noreply.github.com>
2023-07-27 16:06:27 +0100

    Add ball and remove temporary models
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Zombie Football/Assets/Scripts/PlayerMovement.cs
+++ Zombie Football/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform hips;
     public float speed;
+    
+    private Rigidbody _rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed,0,Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Translate(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0,Input.GetAxis("Vertical")*speed*Time.deltaTime);
     }
 }
