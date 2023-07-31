22f59f8aceb2f6942072fa64ee3814cfd9ccfd67
uoethan <72795247+uoethan@users.noreply.github.com>
2023-07-31 10:34:20 +0100

    Add timer

--- Zombie Football/Assets/Scripts/GameController.cs
+++ Zombie Football/Assets/Scripts/GameController.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] GameObject[] goals = new GameObject[2];
     [SerializeField] private GameObject scoreTextObject;
+    [SerializeField] private GameObject timeTextObject;
     
     private readonly GoalChecker[] _goalCheckers = new GoalChecker[2];
     private readonly int[] _scores = { 0, 0 };
     private TextMeshProUGUI _scoreMesh;
+    private TextMeshProUGUI _timeMesh;
+
+    private float _time = 0f;
 
     void Start()
     {
@@ -22,8 +29,14 @@ public class GameController : MonoBehaviour
         }
 
         _scoreMesh = scoreTextObject.GetComponent<TextMeshProUGUI>();
+        _timeMesh = timeTextObject.GetComponent<TextMeshProUGUI>();
     }
-    
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+    }
+
     void FixedUpdate()
     {
         for (int i = 0; i < _goalCheckers.Length; i++)
@@ -35,11 +48,18 @@ public class GameController : MonoBehaviour
                 _goalCheckers[i].goalScored = false;
             }
         }
+        
+        UpdateTimeText();
     }
 
     private void UpdateScoreText()
     {
         _scoreMesh.text = _scores[0] + " - " + _scores[1];
+    }
+
+    private void UpdateTimeText()
+    {
+        _timeMesh.text = Mathf.Floor(_time / 60) + ":" + (Math.Floor(_time % 60)<10 ? "0" : "") + Math.Floor(_time % 60);
     }
 }
 
